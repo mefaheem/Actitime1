712a08f786e05a5ff42909b3cdd90df68b5a4f97
mefaheem <129170884+mefaheem@users.noreply.github.com>
2023-06-02 17:15:11 +0530

    Create deletemodule.java

---
+++ Automation1/src/com/actitime/testscript/deletemodule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class deletemodule {
+	@Test
+	public void deletemodule() {
+		Reporter.log("bye",true);
+	}
+
+}
