84523a2a14f6db636434777001bac4b039e02ba6
acer <acer@DESKTOP-QEQU177>
2023-06-02 17:10:11 +0530

    create module test script

--- Automation1/src/com/actitime/testscript/Createmodule.java
+++ Automation1/src/com/actitime/testscript/Createmodule.java
@@ -1,5 +1,12 @@
 package com.actitime.testscript;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Createmodule {
+	@Test
+	public void createmodule() {
+		Reporter.log("hai",true);
+	}
 
 }
