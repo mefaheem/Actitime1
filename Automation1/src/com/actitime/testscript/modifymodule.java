a34b2b174fbb6643f1b45cac5471b81925a88b33
mefaheem <129170884+mefaheem@users.noreply.github.com>
2023-06-02 17:27:49 +0530

    Create modifymodule.java

---
+++ Automation1/src/com/actitime/testscript/modifymodule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class modifymodule {
+	@Test
+	public void modifymodule() {
+		Reporter.log("bye",true);
+	}
+
+}
