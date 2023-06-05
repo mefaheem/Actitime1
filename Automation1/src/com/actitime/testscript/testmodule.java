ca2871d64c36a4dbcfdae5bc3224214efe2dd10d
acer <acer@DESKTOP-QEQU177>
2023-06-05 16:22:16 +0530

    actitime1

---
+++ Automation1/src/com/actitime/testscript/testmodule.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testmodule {
+	@Test
+	public void createmodule() {
+		Reporter.log("hi",true);
+	}
+
+}
+
+
